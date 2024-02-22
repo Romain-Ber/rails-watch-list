a89b864acfa3bbe2f2b3a5d883d231cdb281602e
Romain Bernier <romain.bernier@live.fr>
2024-02-22 16:43:00 +0100

    rails watch list

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,44 @@
+class BookmarksController < ApplicationController
+
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @movie = Movie.find(params[:bookmark][:movie_id])
+    @bookmark = Bookmark.new(params_bookmark)
+    @bookmark.list = @list
+    @bookmark.movie = @movie
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+
+    redirect_to list_path(@bookmark.list), status: :see_other
+  end
+
+  private
+
+  def set_bookmark
+    @bookmark = bookmark.find(params[:id])
+  end
+
+  def params_bookmark
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
+end
